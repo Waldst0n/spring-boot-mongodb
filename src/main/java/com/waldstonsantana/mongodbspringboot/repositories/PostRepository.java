9e39245706c36d9e0b6b2d6273497b2706c66815
Waldston Santana <waldston.ws@gmail.com>
2024-02-09 11:51:22 -0300

    Simple query with @query

--- src/main/java/com/waldstonsantana/mongodbspringboot/repositories/PostRepository.java
+++ src/main/java/com/waldstonsantana/mongodbspringboot/repositories/PostRepository.java
@@ -2,10 +2,15 @@ package com.waldstonsantana.mongodbspringboot.repositories;
 
 import com.waldstonsantana.mongodbspringboot.models.PostModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<PostModel, String> {
 
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<PostModel> searchTitle(String text);
     List<PostModel> findByTitleContainingIgnoreCase(String text);
+
 }
